e25dfbd922f8e96a783cbb0b355f9c647705c62f
Zachary Ware <zachary.ware@gmail.com>
2017-02-03 23:01:47 -0600

    Factor out custom build step definitions

---
+++ master/custom/steps.py
@@ -0,0 +1,95 @@
+import re
+
+from buildbot.steps.shell import ShellCommand, Test as BaseTest
+
+
+class Test(BaseTest):
+    # Regular expression used to catch warnings, errors and bugs
+    warningPattern = (
+        # regrtest saved_test_environment warning:
+        # Warning -- files was modified by test_distutils
+        # test.support @reap_threads:
+        # Warning -- threading_cleanup() failed to cleanup ...
+        r"Warning -- ",
+        # Py_FatalError() call
+        r"Fatal Python error:",
+        # PyErr_WriteUnraisable() exception: usually, error in
+        # garbage collector or destructor
+        r"Exception ignored in:",
+        # faulthandler_exc_handler(): Windows exception handler installed with
+        # AddVectoredExceptionHandler() by faulthandler.enable()
+        r"Windows fatal exception:",
+        # Resource warning: unclosed file, socket, etc.
+        # NOTE: match the "ResourceWarning" anywhere, not only at the start
+        r"ResourceWarning",
+        # regrtest: At least one test failed. Log a warning even if the test
+        # passed on the second try, to notify that a test is unstable.
+        r'Re-running failed tests in verbose mode',
+        # Thread last resort exception handler in t_bootstrap()
+        r'Unhandled exception in thread started by ',
+    )
+    # Use ".*" prefix to search the regex anywhere since stdout is mixed
+    # with stderr, so warnings are not always written at the start
+    # of a line. The log consumer calls warningPattern.match(line)
+    warningPattern = r".*(?:%s)" % "|".join(warningPattern)
+    warningPattern = re.compile(warningPattern)
+
+    # if tests have warnings, mark the overall build as WARNINGS (orange)
+    warnOnWarnings = True
+
+
+class Clean(ShellCommand):
+    name = "clean"
+    warnOnFailure = 1
+    description = ["cleaning"]
+    descriptionDone = ["clean"]
+    command = ["make", "distclean"]
+    alwaysRun = True
+
+
+class Install(ShellCommand):
+    name = "install"
+    description = ["installing"]
+    descriptionDone = ["Installed"]
+    command = ["make", "install"]
+    haltOnFailure = True
+
+
+class LockInstall(ShellCommand):
+    name = "chmod"
+    description = ["changing permissions"]
+    descriptionDone = ["made install dir unwritable"]
+    command = ["chmod", "-R", "-w", "target/"]
+
+
+class Uninstall(ShellCommand):
+    name = "uninstall"
+    description = ["uninstalling"]
+    descriptionDone = ["Uninstalled"]
+    command = "chmod -R +w target/ &&  rm -rf target/"
+    alwaysRun = True
+    usePTY = False
+
+
+class DocBuild(ShellCommand):
+    name = 'docbuild'
+    description = ['building']
+    descriptionDone = ['build']
+    warnOnWarnings = True
+    command = ['make', '-C', 'Doc/', 'html']
+
+
+class DocLint(ShellCommand):
+    name = 'lint'
+    description = ['linting']
+    descriptionDone = ['lint']
+    warnOnWarnings = True
+    command = ['make', '-C', 'Doc/', 'check']
+
+
+class DocSuspicious(ShellCommand):
+    name = 'suspicious'
+    description = ['checking']
+    descriptionDone = ['suspicious']
+    warnOnWarnings = True
+    command = ['make', '-C', 'Doc/', 'suspicious']
