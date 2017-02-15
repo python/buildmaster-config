0916c999f9a10ef03283f4ed16b08a6235bb615f
Zachary Ware <zachary.ware@gmail.com>
2017-02-15 11:36:31 -0600

    Change link improvements
    - Case insensitive match
    - Also match gh- or pr- prefix to pull request
    - Remove debug check of project name
    - Make title text more explicit

--- master/utils.py
+++ master/utils.py
@@ -5,22 +5,19 @@ from jinja2 import Markup
 
 REPLACEMENTS = [
     (
-        '#(\d+)',
+        '(?<!&)(?:gh-?|pr-?|#)(\d+)(?!;)',
         '<a href="https://github.com/python/cpython/pull/\g<1>"'
-        ' title="Pull Request \g<0>">\g<0></a>'
+        ' title="GitHub Pull Request #\g<1>">\g<0></a>'
     ),
     (
         'bpo-(\d+)',
         '<a href="https://bugs.python.org/issue\g<1>"'
-        ' title="Issue #\g<1>">\g<0></a>'
+        ' title="bugs.python.org Issue #\g<1>">\g<0></a>'
     ),
 ]
 
 
 def changecommentlink(changetext, project_name):
-    if project_name != 'Python':    # XXX temp debugging
-        from twisted.python import log
-        log.msg('got project name %r' % (project_name,))
     for pat, repl in REPLACEMENTS:
-        changetext = re.sub(pat, Markup(repl), changetext)
+        changetext = re.sub(pat, Markup(repl), changetext, flags=re.I)
     return changetext
