e9394fde810b01b5ecb15eae831ca410b5f4bc20
Zachary Ware <zach@python.org>
2019-10-19 22:39:18 -0500

    Switch to a secret config file
    ...rather than a secret Python module.  The config file contains only
    the minimum data to produce the config we want, and default values are
    provided for any missing values (with a warning logged when doing so).
    This also allows for setting worker passwords on a per-owner basis,
    meaning new workers from an existing owner can be added completely via
    public PR.

---
+++ master/custom/settings.py
@@ -0,0 +1,79 @@
+import os
+import secrets
+
+import yaml
+from twisted.python import log
+
+
+DEFAULTS = dict(
+    web_port=9011,
+    worker_port=9021,
+    irc_channel='#buildbot-test',
+    irc_nick='py-bb-test',
+    buildbot_url='http://localhost:9011/',
+    db_url='sqlite:///state.sqlite',
+    do_auth=True,
+    send_mail=False,
+    verbosity=1,
+    git_url='https://github.com/python/cpython',
+)
+
+
+class Settings:
+
+    value = ...
+    path = None
+
+    def __init__(self, value=..., path=None):
+        if value is not ...:
+            self.value = value
+        self.path = path
+
+    @classmethod
+    def from_file(cls, filename):
+        with open(filename) as f:
+            return cls(yaml.full_load(f), path=[])
+
+    def __getitem__(self, key):
+        path_key = real_key = key
+        if isinstance(key, type(self)):
+            path_key = real_key = key.value
+            if real_key is ...:
+                real_key = 'unknown'
+                path_key = key.path
+        new_path = self.path + [path_key]
+        if self.value is not ... and real_key in self.value:
+            value = self.value[real_key]
+            if isinstance(value, (list, dict)):
+                return type(self)(value, path=new_path)
+            return value
+        return type(self)(path=new_path)
+
+    __getattr__ = __getitem__
+
+    def get(self, key, default=...):
+        if self.value is not ...:
+            value = self.value.get(key, default)
+            if value is not ...:
+                return value
+        if default is not ...:
+            return default
+        return type(self)(path=self.path + [key])
+
+    def _convert(self, func, default):
+        if self.value is not ...:
+            return func(self.value)
+        default = DEFAULTS.get('.'.join(map(str, self.path)), default)
+        if not os.getenv('CI'):
+            # Note: We use log.err to make this show up during `checkconfig`
+            log.err(f'WARNING: No setting at {self.path}, returning {default}')
+        return func(default)
+
+    def __int__(self):
+        return self._convert(int, 1)
+
+    def __str__(self):
+        return self._convert(str, secrets.token_urlsafe(8))
+
+    def __bool__(self):
+        return self._convert(bool, False)
---
+++ master/custom/workers.py
@@ -0,0 +1,165 @@
+# -*- python -*-  vi:ft=python:
+# kate: indent-mode python; hl python;
+# vim:set ts=8 sw=4 sts=4 et:
+
+from functools import partial
+
+from buildbot.plugins import worker as _worker
+
+
+class CPythonWorker:
+
+    def __init__(self, settings, name,
+                 tags=None, branches=None,
+                 parallel_builders=None, parallel_tests=None):
+        self.name = name
+        self.tags = tags or set()
+        self.branches = branches
+        self.parallel_builders = parallel_builders
+        self.parallel_tests = parallel_tests
+        worker_settings = settings.workers[name]
+        owner = name.split('-')[0]
+        owner_settings = settings.owners[owner]
+        pw = worker_settings.get('password', None) or owner_settings.password
+        owner_email = owner_settings.get('email', None)
+        emails = list(map(str, filter(None, (settings.get('status_email', None), owner_email))))
+        self.bb_worker = _worker.Worker(name, str(pw), notify_on_missing=emails)
+
+
+def get_workers(settings):
+    cpw = partial(CPythonWorker, settings)
+    return [
+        cpw(
+            name="aixtools-aix-power6",
+            tags=['aix', 'unix', 'power6'],
+            branches=['3.8', '3.x'],
+        ),
+        cpw(
+            name="angelico-debian-amd64",
+            tags=['linux', 'unix', 'debian', 'amd64', 'x86-64'],
+        ),
+        cpw(
+            name="billenstein-sierra",
+            tags=['macOS', 'unix', 'sierra', 'amd64', 'x86-64'],
+        ),
+        cpw(
+            name="bolen-ubuntu",
+            tags=['linux', 'unix', 'ubuntu', 'amd64', 'x86-64'],
+        ),
+        cpw(
+            name="bolen-windows10",
+            tags=['windows', 'win10', 'amd64', 'x86-64'],
+        ),
+        cpw(
+            name="bolen-windows7",
+            tags=['windows', 'win7', 'x86'],
+        ),
+        cpw(
+            name="bolen-windows",
+            tags=['windows', 'winXP', 'x86'],
+            branches=['2.7'],
+        ),
+        cpw(
+            name="bray-win10-cygwin-amd64",
+            tags=['windows', 'win10', 'cygwin', 'unix', 'amd64', 'x86-64'],
+            branches=['3.x'],
+        ),
+        cpw(
+            name="cstratak-fedora-rawhide-x86_64",
+            tags=['linux', 'unix', 'fedora', 'amd64', 'x86-64'],
+            parallel_tests=10,
+        ),
+        cpw(
+            name="cstratak-fedora-stable-x86_64",
+            tags=['linux', 'unix', 'fedora', 'amd64', 'x86-64'],
+            parallel_tests=10,
+        ),
+        cpw(
+            name="cstratak-RHEL7-x86_64",
+            tags=['linux', 'unix', 'rhel', 'amd64', 'x86-64'],
+            parallel_tests=10,
+        ),
+        cpw(
+            name="cstratak-RHEL8-x86_64",
+            tags=['linux', 'unix', 'rhel', 'amd64', 'x86-64'],
+            parallel_tests=10,
+        ),
+        cpw(
+            name="edelsohn-aix-ppc64",
+            tags=['aix', 'unix', 'ppc64'],
+            branches=['3.8', '3.x'],
+        ),
+        cpw(
+            name="edelsohn-debian-z",
+            tags=['linux', 'unix', 'debian', 's390x'],
+        ),
+        cpw(
+            name="edelsohn-fedora-ppc64",
+            tags=['linux', 'unix', 'fedora', 'ppc64'],
+        ),
+        cpw(
+            name="edelsohn-fedora-ppc64le",
+            tags=['linux', 'unix', 'fedora', 'ppc64le'],
+        ),
+        cpw(
+            name="edelsohn-sles-z",
+            tags=['linux', 'unix', 'sles', 's390x'],
+        ),
+        cpw(
+            name="edelsohn-rhel-z",
+            tags=['linux', 'unix', 'rhel', 's390x'],
+        ),
+        cpw(
+            name="einat-ubuntu",
+            tags=['linux', 'unix', 'ubuntu', 'amd64', 'x86-64'],
+        ),
+        cpw(
+            name="gps-clang-ubsan",
+            tags=['linux', 'unix', 'amd64', 'x86-64'],
+        ),
+        cpw(
+            name="gps-debian-profile-opt",
+            tags=['linux', 'unix', 'debian', 'amd64', 'x86-64'],
+        ),
+        cpw(
+            name="gps-ubuntu-exynos5-armv7l",
+            tags=['linux', 'unix', 'ubuntu', 'arm', 'armv7', 'arm32'],
+            parallel_tests=8,
+        ),
+        cpw(
+            name="kloth-win64",
+            tags=['windows', 'win7', 'amd64', 'x86-64'],
+            parallel_builders=2,
+            parallel_tests=4,
+        ),
+        cpw(
+            name="koobs-freebsd-9e36",
+            tags=['bsd', 'unix', 'freebsd', 'amd64', 'x86-64'],
+            parallel_tests=4,
+        ),
+        cpw(
+            name="koobs-freebsd-564d",
+            tags=['bsd', 'unix', 'freebsd', 'amd64', 'x86-64'],
+            parallel_tests=4,
+        ),
+        cpw(
+            name="monson-win-arm32",
+            tags=['windows', 'win10', 'arm', 'arm32'],
+            branches=['3.x'],
+        ),
+        cpw(
+            name="ware-alpine",
+            tags=['linux', 'unix', 'alpine', 'docker', 'amd64', 'x86-64'],
+            branches=['3.x'],
+        ),
+        cpw(
+            name="ware-gentoo-x86",
+            tags=['linux', 'unix', 'gentoo', 'x86'],
+            parallel_builders=2,
+        ),
+        cpw(
+            name="ware-win81-release",
+            tags=['windows', 'win8', 'amd64', 'x86-64'],
+            parallel_tests=4,
+        ),
+    ]
