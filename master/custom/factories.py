630e74094a266d7ee706e90e1177a917be0f7584
Pablo Galindo <Pablogsal@gmail.com>
2020-09-05 17:06:30 +0100

    Skip test_ctypes in the ASAN builder, mark it as stable (#214)

--- master/custom/factories.py
+++ master/custom/factories.py
@@ -224,8 +224,9 @@ class UnixAsanBuild(UnixBuild):
     configureFlags = ["--with-pydebug", "--without-pymalloc", "--with-address-sanitizer"]
     factory_tags = ["asan", "sanitizer"]
     test_environ = {'ASAN_OPTIONS': 'detect_leaks=0'}
-    testFlags = ("-j2 -x test_capi test_crypt test_decimal "
-                 "test_faulthandler test_interpreters")
+    # These tests are currently raising false positives or are interfering with the ASAN mechanism,
+    # so we need to skip them unfortunately.
+    testFlags = ("-j1 -x test_ctypes test_capi test_crypt test_decimal test_faulthandler test_interpreters")
 
 
 class UnixBuildWithoutDocStrings(UnixBuild):
