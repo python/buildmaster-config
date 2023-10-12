116f7c50064e2f3642d35b57f3de9221f4b83b59
Victor Stinner <vstinner@python.org>
2023-10-12 12:05:05 +0200

    Cache Release Status result for 5 minutes (#423)
    Co-authored-by: Łukasz Langa <lukasz@langa.pl>

--- master/custom/release_dashboard.py
+++ master/custom/release_dashboard.py
@@ -1,20 +1,25 @@
+import datetime
 import os
+import time
 
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 from buildbot.data.resultspec import Filter
 
 FAILED_BUILD_STATUS = 2
 
+# Cache result for 5 minutes. Generating the page is slow and a Python build
+# takes at least 5 minutes, a common build takes 10 to 30 minutes.
+CACHE_DURATION = 5 * 60
+
 
 def get_release_status_app(buildernames):
     release_status_app = Flask("test", root_path=os.path.dirname(__file__))
-
     buildernames = set(buildernames)
+    cache = None
 
-    @release_status_app.route("/index.html")
-    def main():
+    def get_release_status():
         builders = release_status_app.buildbot_api.dataGet("/builders")
 
         failed_builds_by_branch = {}
@@ -53,7 +58,28 @@ def get_release_status_app(buildernames):
 
             failed_builds_by_branch[branch].append((builder, last_build))
 
+        generated_at = datetime.datetime.now(tz=datetime.timezone.utc)
         failed_builders = sorted(failed_builds_by_branch.items(), reverse=True)
-        return render_template("releasedashboard.html", failed_builders=failed_builders)
+        return render_template(
+            "releasedashboard.html",
+            failed_builders=failed_builders,
+            generated_at=generated_at,
+        )
+
+    @release_status_app.route("/index.html")
+    def main():
+        nonlocal cache
+
+        force_refresh = request.args.get("refresh", "").lower() in {"1", "yes", "true"}
+
+        if cache is not None and not force_refresh:
+            result, deadline = cache
+            if time.monotonic() <= deadline:
+                return result
+
+        result = get_release_status()
+        deadline = time.monotonic() + CACHE_DURATION
+        cache = (result, deadline)
+        return result
 
     return release_status_app
