e625a6cdd8c3f25bac1f92282952b4527cfbec4a
Itamar Oren <itamarost@gmail.com>
2024-09-23 15:31:21 -0700

    gh-522: Promote my CentOS9 builders to stable, remove ubuntu worker

--- master/custom/workers.py
+++ master/custom/workers.py
@@ -258,13 +258,6 @@ def get_workers(settings):
             parallel_tests=4,
         ),
         cpw(
-            name="itamaro-ubuntu-aws",
-            tags=['linux', 'unix', 'ubuntu', 'amd64', 'x86-64'],
-            not_branches=['3.9', '3.10', '3.11', '3.12'],
-            parallel_tests=10,
-            parallel_builders=2,
-        ),
-        cpw(
             name="itamaro-centos-aws",
             tags=['linux', 'unix', 'rhel', 'amd64', 'x86-64'],
             not_branches=['3.9', '3.10', '3.11', '3.12'],
