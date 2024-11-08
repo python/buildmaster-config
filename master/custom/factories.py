62ba4ead2a8abbea56a48c89deb45228fde9f41f
Brett Cannon <brett@python.org>
2024-11-07 16:24:02 -0800

    Fix WASI buildbots to support the new default target (#556)
    Brand everything as being for WASI preview1

--- master/custom/factories.py
+++ master/custom/factories.py
@@ -872,7 +872,7 @@ class Wasm32WasiCrossBuild(UnixCrossBuild):
         super().setup(parallel, branch, test_with_PTY=test_with_PTY, **kwargs)
 
 
-class _Wasm32WasiBuild(UnixBuild):
+class _Wasm32WasiPreview1Build(UnixBuild):
     """Build Python for wasm32-wasi using Tools/wasm/wasi.py."""
     buildersuffix = ".wasi"
     factory_tags = ["wasm", "wasi"]
@@ -886,7 +886,8 @@ class _Wasm32WasiBuild(UnixBuild):
 
     def setup(self, parallel, branch, test_with_PTY=False, **kwargs):
         wasi_py = "Tools/wasm/wasi.py"
-        host_path = "build/cross-build/wasm32-wasi"
+        host_triple = "wasm32-wasip1"
+        host_path = f"build/cross-build/{host_triple}"
 
         # Build Python
         build_configure = ["python3", wasi_py, "configure-build-python"]
@@ -968,7 +969,7 @@ class _Wasm32WasiBuild(UnixBuild):
 
 # Preventing this from running on versions older than 3.13 is managed in
 # master.cfg.
-class Wasm32WasiDebugBuild(_Wasm32WasiBuild):
+class Wasm32WasiPreview1DebugBuild(_Wasm32WasiPreview1Build):
     append_suffix = ".debug"
     pydebug = True
     testFlags = ["-u-cpu"]
