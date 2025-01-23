982c88e63fcb2572ecd8b1b008c269f640b22da3
Russell Keith-Magee <russell@keith-magee.com>
2025-01-23 08:06:56 +0800

    Remove parallelization for iOS runners. (#573)

--- master/custom/workers.py
+++ master/custom/workers.py
@@ -282,7 +282,9 @@ def get_workers(settings):
             name="rkm-arm64-ios-simulator",
             tags=['iOS'],
             not_branches=['3.9', '3.10', '3.11', '3.12'],
-            parallel_builders=4,
+            # Starting/running simulators in parallel can be problematic;
+            # see python/cpython#129200
+            parallel_builders=1,
         ),
         cpw(
             name="mhsmith-android-aarch64",
