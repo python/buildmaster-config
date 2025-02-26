python/buildmaster-config

commit bb43ee614ebf2bc7c29601c994544a114e7ae38a

Link to PR-specific grid in PR message (#576)
Also add the link to 'command' PR runs rather than just 'label' runs.

Expansion of #571; ref #327.

Note that, at present, the link does not actually filter to the specified branch.  It is hoped that a later update to buildbot will enable that.

master/custom/pr_testing.py
@@ -15,19 +15,20 @@ REFLEAK_TESTING_LABEL = ":hammer: test-with-refleak-buildbots"
 
 GITHUB_PROPERTIES_WHITELIST = ["*.labels"]
 
-BUILD_SCHEDULED_MESSAGE_TEMPLATE = """\
+BUILD_MESSAGE_HEADER = """\
 :robot: New build scheduled with the buildbot fleet by @{user} for commit {commit} :robot:
 
 Results will be shown at:
 
-https://buildbot.python.org/all/#/grid?branch=refs%2Fpull%2F30617%2Fmerge
+https://buildbot.python.org/all/#/grid?branch=refs%2Fpull%2F{pr_number}%2Fmerge
 
+"""
+
+BUILD_SCHEDULED_MESSAGE_TEMPLATE = BUILD_MESSAGE_HEADER + """\
 If you want to schedule another build, you need to add the <kbd>{label}</kbd> label again.
 """
 
-BUILD_COMMAND_SCHEDULED_MESSAGE_TEMPLATE = """\
-:robot: New build scheduled with the buildbot fleet by @{user} for commit {commit} :robot:
-
+BUILD_COMMAND_SCHEDULED_MESSAGE_TEMPLATE = BUILD_MESSAGE_HEADER + """\
 The command will test the builders whose names match following regular expression: `{filter}`
 
 The builders matched are:
@@ -83,11 +84,15 @@ class CustomGitHubEventHandler(GitHubEventHandler):
         url = payload["pull_request"]["comments_url"]
         username = payload["sender"]["login"]
         commit = payload["pull_request"]["head"]["sha"]
+        pr_number = payload["issue"]["number"]
         yield http.post(
             url.replace(self.github_api_endpoint, ""),
             json={
                 "body": BUILD_SCHEDULED_MESSAGE_TEMPLATE.format(
-                    user=username, commit=commit, label=label
+                    user=username,
+                    commit=commit,
+                    label=label,
+                    pr_number=pr_number,
                 )
             },
         )
@@ -281,6 +286,7 @@ class CustomGitHubEventHandler(GitHubEventHandler):
                 user=payload["sender"]["login"],
                 commit=head_sha,
                 filter=builder_filter,
+                pr_number=number,
                 builders="\n".join(
                     {
                         f"- `{builder}`"

commit e2c376898d5abe7466292067d1cc59c07fa55a2c

Fix test-with-buildbots label handling
Fixes #578, a bug introduced by #576 for #327.

I was mislead by the number extraction in another part of the file.

master/custom/pr_testing.py
@@ -84,7 +84,7 @@ class CustomGitHubEventHandler(GitHubEventHandler):
         url = payload["pull_request"]["comments_url"]
         username = payload["sender"]["login"]
         commit = payload["pull_request"]["head"]["sha"]
-        pr_number = payload["issue"]["number"]
+        pr_number = payload["pull_request"]["number"]
         yield http.post(
             url.replace(self.github_api_endpoint, ""),
             json={