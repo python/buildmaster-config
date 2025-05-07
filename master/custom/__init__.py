90582289304ea47534876c36ae08c04dfcae9913
Łukasz Langa <lukasz@langa.pl>
2025-05-07 18:59:33 +0200

    Add 3.14 branch, update main to 3.15 (#594)

--- master/custom/__init__.py
+++ master/custom/__init__.py
@@ -1,4 +1,4 @@
-MAIN_BRANCH_VERSION = "3.14"
+MAIN_BRANCH_VERSION = "3.15"
 # The Git branch is called "main", but we give it a different name in buildbot.
 # See git_branches in master/master.cfg.
 MAIN_BRANCH_NAME = "3.x"
