c8d6940f73092d1c17f9f4691fce07c205b92d3d
Victor Stinner <vstinner@python.org>
2026-01-30 13:26:49 +0100

    Apply suggestion from @StanFromIreland
    Co-authored-by: Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>

--- master/custom/builders.py
+++ master/custom/builders.py
@@ -309,8 +309,6 @@ UNSTABLE_BUILDERS_TIER_3 = [
     # FreeBSD x86-64 clang
     # FreeBSD 15 is CURRENT: development branch (at 2023-10-17)
     ("AMD64 FreeBSD15", "opsec-fbsd15", UnixBuild),
-
-    # FreeBSD x86-64 clang
     # FreeBSD 16 is CURRENT: development branch (at 2026-01-09)
     ("AMD64 FreeBSD16", "opsec-fbsd16", UnixBuild),
 
