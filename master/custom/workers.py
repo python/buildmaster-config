9d5cfa4ba2658566c7980d4f3a0f0de1771d2720
Stan Ulbrych <89152624+StanFromIreland@users.noreply.github.com>
2026-02-19 05:10:35 +0000

    Add `stan-aarch64-ubuntu` to run `test_xpickle` (#664)

--- master/custom/workers.py
+++ master/custom/workers.py
@@ -185,6 +185,13 @@ def get_workers(settings):
             parallel_tests=4,
         ),
         cpw(
+            name="stan-aarch64-ubuntu",
+            tags=['linux', 'unix', 'ubuntu', 'arm', 'arm64', 'aarch64'],
+            parallel_tests=4,
+            # test_xpickle doesn't exist on these branches
+            not_branches=['3.12', '3.11', '3.10'],
+        ),
+        cpw(
             name="stan-raspbian",
             tags=['linux', 'unix', 'raspbian', 'debian', 'armv8',
                   'aarch64', 'arm'],
