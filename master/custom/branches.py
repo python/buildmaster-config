5c90b0d8e7753ed2af87c75710e59fdbc527d51e
Zachary Ware <zach@python.org>
2026-05-07 11:16:18 -0500

    Add 3.15 branch, bump main to 3.16

--- master/custom/branches.py
+++ master/custom/branches.py
@@ -23,13 +23,14 @@ from typing import Any
 def generate_branches():
     yield BranchInfo(
         '3.x',
-        version_tuple=(3, 15),
+        version_tuple=(3, 16),
         git_branch='main',
         is_main=True,
         builddir_name='3.x',
         builder_tag='3.x',
         sort_key=-9999,
     )
+    yield _maintenance_branch(3, 15)
     yield _maintenance_branch(3, 14)
     yield _maintenance_branch(3, 13)
     yield _maintenance_branch(3, 12)
